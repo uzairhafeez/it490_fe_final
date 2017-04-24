9db8b2ab822d17c31730c068a9b0f627e7faf37b
uzairhafeez <muh3@njit.edu>
2017-04-24 13:37:39 -0400

    Updated FE Package Script

--- package_FE.sh
+++ package_FE.sh
@@ -1,10 +1,13 @@
 echo "FE Bundle"
+	
+	cd /home/uzair/frontEnd/tmp
 	DIR=/home/uzair/FE
         cp -a $DIR* /home/uzair/frontEnd/tmp/
         tar -czvf frontendPackage.tgz -C /home/uzair/frontEnd/tmp/FE .
-        echo "New Package: `ls | grep frontendPackage`"
+        # echo "New Package: `ls | grep frontendPackage`"
 
-	echo "tar -xf $HOME/Desktop/frontendPackage.tgz -C /home/uzair/Desktop/Unzip/" >> deploy.sh
+	# echo "tar -xf $HOME/Desktop/frontendPackage.tgz -C /home/uzair/Desktop/Unzip/" >> deploy.sh
+	
 	# cd /home/uzair/Desktop/Unzip/
 	
 	# echo "mv 000-default.conf.back /etc/apache2/sites-available/" >> deploy.sh
@@ -16,3 +19,7 @@ echo "FE Bundle"
         echo Success
 
 	echo "Bundle is ready to deploy"
+	
+	scp -r /home/uzair/frontEnd/tmp/frontendPackage.tgz uzair@192.168.2.30:/home/uzair/Packages/frontend
+	rm /home/uzair/frontEnd/tmp/frontendPackage.tgz
+	
